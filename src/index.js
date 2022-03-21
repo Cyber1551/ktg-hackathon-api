620e61603e2a81d3b84905363553494c5993eae6
Brandon Lacy <blacy1551@gmail.com>
2022-03-21 11:33:06 -0500

    final fix maybe?

--- src/index.js
+++ src/index.js
@@ -9,23 +9,9 @@ const app = express();
 // using bodyParser to parse JSON bodies into JS objects
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested, Content-Type, Accept Authorization"
-    )
-    if (req.method === "OPTIONS") {
-        res.header(
-            "Access-Control-Allow-Methods",
-            "POST, PUT, PATCH, GET, DELETE"
-        )
-        return res.status(200).json({})
-    }
-    next()
-})
-
-app.use(cors())
+app.use(cors({
+    origin: 'https://ktg-hackathon-frontend.herokuapp.com'
+}));
 
 app.get('/', (req, res) => res.send('Working!!!'));
 
