55c908d9b785d08d77ae906430e7784bb3e5bfcf
Brandon Lacy <blacy1551@gmail.com>
2022-03-21 11:19:53 -0500

    added accepted methods cors

--- src/index.js
+++ src/index.js
@@ -9,10 +9,18 @@ const app = express();
 // using bodyParser to parse JSON bodies into JS objects
 app.use(bodyParser.json());
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
+
 app.use(
     cors()
 );
 app.options('*', cors());
+
 
 app.get('/', (req, res) => res.send('Working!!!'));
 
